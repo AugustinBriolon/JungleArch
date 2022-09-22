dbb340c05b548732b0b10c945ca5dfd26eebaaea
« Matteo <matteo.courquin@gmail.com>
2022-09-22 12:23:38 +0200

    :constrcution: Header && section title

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex justify-between items-center w-full">
+  <div class="header flex justify-between items-center w-full" ref="header">
     <nuxt-img src="/images/logos/logo-jungleArch.png"></nuxt-img>
     <nav>
       <ul class="text-white list-style-none flex">
-        <li class="pr-20">Menu</li>
-        <li class="pr-20">Ambiance</li>
-        <li class="pr-20">Plats</li>
-        <li>Avis</li>
+        <li class="pr-20"><a href="#" class="links">Menu</a></li>
+        <li class="pr-20"><a href="#" class="links">Ambiance</a></li>
+        <li class="pr-20"><a href="#" class="links">Plats</a></li>
+        <li><a href="#" class="links">Avis</a></li>
       </ul>
     </nav>
   </div>
@@ -16,13 +16,31 @@
 
 export default {
   data() {
-    return {};
+    return {}
   },
-  methods: {}
+  methods: {},
+  mounted () {
+    const header = document.querySelector('.header');
+    window.addEventListener("scroll", function () {
+      header.classList.toggle("sticky", window.scrollY > 100);
+    });
+  },
 }
 
 </script>
 
 <style scoped lang='scss'>
+
+.header{
+  max-width: 1920px;
+  padding: 0 clamp(20px, 3vw, 100px);
+  z-index: 99999;
+  transition: .3s;
+}
+
+.sticky{
+  background: rgba(0, 0, 0, 0.6);
+  position: fixed;
+}
 
 </style>
